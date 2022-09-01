c1089e70daefaa4f2d00b876ecfc4e4789cb5a26
8-Lambda-8 <j.wasle111@gmail.com>
2022-09-01 23:30:20 +0200

    redesigned port asignment logic

--- chromeTabLinker.py
+++ chromeTabLinker.py
@@ -11,20 +11,36 @@ for i, p in enumerate(mix_chrome_ports):
     mix_chrome_ports[i] = (client.get_port_by_name(p[0]),
                            client.get_port_by_name(p[1]))
 
+tempPort = None
+
 @client.set_port_registration_callback
-def port_registration(port: jack.Port, register):
+def port_registration(port, register):
 
     if "Google Chrome" in port.name and register:
+
+        global tempPort
+        if tempPort is None:
+            tempPort = port
+            return
 
         for conPorts in client.get_all_connections(port):
             client.disconnect(port, conPorts)
 
         LR = 0 if port.name[-1] == 'L' else 1
 
-        for i, p in enumerate(mix_chrome_ports):
-            if(len(client.get_all_connections(p[LR])) == 0):
-                client.connect(port, mix_chrome_ports[i][LR])
-                break
+        lessUsedPorts = min(mix_chrome_ports, key=lambda x:len(client.get_all_connections(x[0])))
+        print(lessUsedPorts)
+
+        if port.name[-1] == 'L':
+            client.connect(port, lessUsedPorts[0])
+            client.connect(tempPort, lessUsedPorts[1])
+        elif port.name[-1] == 'R':
+            client.connect(port, lessUsedPorts[1])
+            client.connect(tempPort, lessUsedPorts[0])
+        else:
+            print("port name does not end with L or R")
+
+        tempPort = None
 
 with client:
     while True:
